e64745759d8db30d978ae1991a7e0e91fc6c7e29
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-06-22 18:36:59 +0630

    hero section and aboutMe
    added hero section and aboutMe

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Hero from './sections/Hero'
 
 const App = () => {
   return (
-    <div>
-        <h1 className='text-5xl text-white-50'>Let's build a 3d potfolio</h1>
-    </div>
+    <>
+      <Hero />
+    </>
+
   )
 }
 
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { aboutMe, greetings, words } from '../../constants'
+import Button from '../components/Button'
+
+const Hero = () => {
+    return (
+        <section id='hero' className="relative overflow-hidden">
+            <div className="absolute top-0 left-0 z-10">
+                <img src="/images/bg.png" alt="background" />
+            </div>
+            <div className="hero-layout">
+                {/* left:HERO CONTENT  */}
+                <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+                    <div className="flex flex-col gap-7">
+                        <div className="hero-text">
+                           <h1>{aboutMe[0].title1} 
+                            <span className='slide'>
+                                <span className="wrapper">
+                                                                {words.map((word) => (
+                               <span key={word.id} className='flex items-center md:gap-3 gap-1 pb-2'>
+                                <img src={word.imgPath} alt={word.text}
+                                className='xl:size-12 md:size-10 size-7 md:p-2 p-1 bg-white-50 rounded-full' />
+                                <span>{word.text}</span>
+                               </span>
+                            ))}
+                                </span>
+                            </span>
+                           </h1>
+                           <h1>{aboutMe[1].title2}</h1>
+                           <h1>{aboutMe[2].title3}</h1>
+                           <p className='text-white-50 md:text-xl relative z-10 pointer-events-none'>{greetings.text}</p>
+                           <Button/>
+                        </div>
+                    </div>
+                </header>
+                {/* right:HERO 3D Model */}
+            </div>
+        </section>
+    )
+}
+
+export default Hero
