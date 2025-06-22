6d68083422b081cc3c57ae34d5a32e487b598e02
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-06-22 17:22:10 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1 className='text-5xl text-white-50'>Let's build a 3d potfolio</h1>
+    </div>
+  )
+}
+
+export default App
