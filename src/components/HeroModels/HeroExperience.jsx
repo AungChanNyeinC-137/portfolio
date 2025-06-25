431fb7e290e617ae59c25c2d9371d88ca2270157
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-06-25 18:57:39 +0630

    navbar
    navbar added with statecontroll of background

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -20,20 +20,20 @@ const HeroExperience = () => {
         enableZoom={!isTablet} // Disables zoom on tablets
         maxDistance={20} // Maximum distance for zooming out
         minDistance={5} // Minimum distance for zooming in
-        minPolarAngle={Math.PI / 6} // Minimum angle for vertical rotation
-        maxPolarAngle={Math.PI / 2} // Maximum angle for vertical rotation
+        minPolarAngle={isMobile ? Math.PI / 3 : Math.PI / 6}
+        maxPolarAngle={isMobile ? Math.PI / 3 : Math.PI / 2}
       />
 
-        <HeroLights />
-        <Particles/>
+      <HeroLights />
+      <Particles />
 
-        <group
-          scale={isMobile ? 2.4  : 3.4}
-          position={[0, -1.2, 0]}
-          rotation={[0, -Math.PI / 4, 0]}
-        >
-          <Computer/>
-          </group>
+      <group
+        scale={isMobile ? 2.4 : 3.4}
+        position={[0, -1.2, 0]}
+        rotation={[0, -Math.PI / 4, 0]}
+      >
+        <Computer />
+      </group>
     </Canvas>
   );
 };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import { navLinks } from '../../constants'
+
+const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+  useEffect( () => {
+    const handleScroll = () =>{
+      const isScrolled = window.scrollY > 10;
+      setScrolled(true);
+    }
+    window.addEventListener('scroll',handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll)
+  },[])
+  return (
+    <header className={`navbar ${scrolled? 'scrolled': 'not-scrolled'}`}>
+      <div className="inner">
+        <a href="#hero" className='logo'>AungChan</a>
+        <nav className="desktop">
+          <ul>
+            {navLinks.map( (navlink) => (
+              <li key={navlink.name} className='group'>
+                <a href={navlink.link}>
+                  <span>{navlink.name}</span>
+                  <span className='underline'/>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+         <a href="#contact" className='contact-btn group'>
+          <div className="inner">
+            <span>Contact Me</span> 
+          </div>
+         </a>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
