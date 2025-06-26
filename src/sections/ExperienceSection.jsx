62133c46ebfd8a438eb4b76695ef99c86abf54d1
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-06-27 00:36:53 +0630

    added experience section
    added experience section with correct logo and scrollTrigger animation

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -2,8 +2,59 @@ import React from 'react'
 import TitleHeader from '../components/TitleHeader'
 import { expCards } from '../../constants'
 import GlowCard from '../components/GlowCard'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger);
 
 const ExperienceSection = () => {
+  useGSAP(() => {
+    gsap.utils.toArray('.timeline-card').forEach((card) => {
+      gsap.from(card, {
+        xPercent: -100,
+        opacity: 0,
+        transformOrigin: 'left left',
+        duration: 1,
+        ease: 'power2.inOut',
+        scrollTrigger: {
+          trigger: card,
+          start: 'top 80%'
+        }
+      })
+    })
+    gsap.to('.timeline', {
+      transformOrigin: 'bottom bottom',
+      ease: 'power2.inOut',
+      scrollTrigger: {
+        trigger: '.timeline',
+        start: 'top 80%',
+        end: '70% center',
+        onUpdate: (self) => {
+          gsap.to('.timeline', {
+            scaleY: 1 - self.progress,
+          })
+        }
+      }
+    })
+    gsap.utils.toArray('.expText').forEach((text) => {
+      gsap.from(text, {
+        xPercent: 0,
+        opacity: 0,
+        duration: 1,
+        ease: 'power2.inOut',
+        scrollTrigger: {
+          trigger: text,
+          start: 'top 60%'
+        }
+      })
+    })
+
+  }, [])
+  const calendar = (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+  </svg>
+
+  )
   return (
     <section id='experience' className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
       <div className="w-full h-full md:px-20 px-5">
@@ -22,10 +73,41 @@ const ExperienceSection = () => {
                 <div className="xl:w-2/6">
 
                   <GlowCard card={card} index={index}>
-                    <div>
+                    <div className='size-32 h-fit bg-gray-300 rounded-sm p-1'>
                       <img src={card.imgPath} alt={card.title} />
                     </div>
                   </GlowCard>
+                </div>
+                <div className='xl:w-4/6 '>
+                  <div className="flex items-start">
+                    <div className="timeline-wrapper">
+                      <div className="timeline" />
+                      <div className='gradient-line w-1 h-full' />
+                    </div>
+                    <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
+                      <div className="timeline-logo">
+                          <img src={card.logoPath} alt="logo" className="w-5 h-5 md:w-7 md:h-7 object-contain" />
+                      </div>
+                      <div>
+                        <h1 className='font-semibold text-3xl'>{card.title}</h1>
+                        <p className="my-4 text-white-50 flex ">
+                          {calendar}&nbsp; {card.date}
+                        </p>
+                        <p className="italic text-[blue-300]">
+                          Responsibilities
+                        </p>
+                        <ul className='list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50'>
+                          {card.responsibilities.map((resp) => (
+                            <li key={resp} className='text-lg'>
+                              {resp}
+
+                            </li>
+                          ))}
+
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             ))}
